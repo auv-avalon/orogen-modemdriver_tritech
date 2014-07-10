b592993700856abcac4fad3a83c3cc47f4acc5d9
Niklas Pech <niklas.pech@dfki.de>
2014-07-10 14:40:28 +0200

    show driver stats

--- tasks/ModemCanbus.cpp
+++ tasks/ModemCanbus.cpp
@@ -2,6 +2,7 @@
 
 #include "ModemCanbus.hpp"
 #include <modemdriver_tritech/ModemParser.hpp>
+#include <modemdriver_tritech/AckDriverStats.hpp>
 
 
 using namespace modemdriver;
@@ -55,6 +56,20 @@ void ModemCanbus::updateHook()
         }
     }
     ack_driver.process();
+    AckDriverStats s = ack_driver.getDriverStats();
+    _stats.write(s);
+    if (count > 10) {
+            std::cout << "Last send Ack Bit:" <<(int) s.last_send_ack_bit << std::endl; 
+            std::cout << "Last receive Ack Bit:" <<(int) s.last_receive_ack_bit << std::endl; 
+            std::cout << "Acked Data Packets:" <<(int) s.acked_data_packets << std::endl; 
+            std::cout << "Acked Protocol Packets:" <<(int) s.acked_protocol_packets << std::endl; 
+            std::cout << "Received Protocol Packets:" <<(int) s.received_protocol_packets << std::endl; 
+            std::cout << "Received Data Packets:" <<(int) s.received_data_packets << std::endl; 
+            std::cout << "Last valid packet (since seconds):" << (base::Time::now()-s.last_valid_packet).toSeconds() << std::endl; 
+            std::cout << "Retry:" <<(int) s.retries << std::endl; 
+            std::cout << "Rejected Packets:" <<(int) s.rejected_packets << std::endl; 
+
+    }
 
 }
 void ModemCanbus::errorHook()
